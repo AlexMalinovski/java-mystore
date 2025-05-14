b9a499994b8d8b8fffff12c37f9a73bc7e633532
Alexey Malinovsky <alexmal08@yandex.ru>
2025-05-15 01:53:59 +0300

    feat: добавить модуль mystore-payment

---
+++ mystore-payment/src/test/java/ru/practicum/mystore/payment/PaymentApplicationTest.java
@@ -0,0 +1,12 @@
+package ru.practicum.mystore.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTest {
+
+    @Test
+    void loadContext() {
+    }
+}
